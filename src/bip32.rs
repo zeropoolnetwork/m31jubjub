b56b2c95dd92e6baf342a5887b08cfabda6a04bc
unegare <dronte2008@yandex.ru>
2024-09-19 00:07:10 +0300

    bip32: experimental derive_priv

--- src/bip32.rs
+++ src/bip32.rs
@@ -15,29 +15,55 @@ pub struct ChildNum {
     is_hardened: bool,
 }
 
+impl ChildNum {
+    pub fn next(&self) -> Self {
+        Self {
+            num: self.num +1,
+            ..*self
+        }
+    }
+}
+
 pub const MASTER_SEED: &[u8;13] = b"Zeropool seed";
 
-pub fn derive_hardened_priv(key: &Fs, chain_code: &[u8], num: u32) -> Option<(Fs, [u8; 32])> {
+pub fn derive_priv(private_key: &Fs, chain_code: &[u8], num: u32) -> Option<(Fs, [u8; 32])> {
 //    let mut buf: [u8; 32] = [0u8; 32];
 //    let mut buf2: [u8; 32] = [0u8; 32];
 //    let x = key.serialize_compressed(&mut buf[..]).ok()?;
 //    let y = key.serialize_uncompressed(&mut buf2[..]).ok()?;
 //    assert_eq!(buf, buf2);
 
-    let mut source: [u8; 1 + 32 + 4] = [0u8; 37];
-    key.serialize_compressed(&mut source[1..33]).unwrap();//.ok()?;
-    let c_num_arr = num.to_be_bytes();
-    source[33..].copy_from_slice(&c_num_arr);
     let mut hmac_obj = Hmac::<Sha512>::new_from_slice(chain_code).unwrap();//.ok()?;
-    hmac_obj.update(&source);
-    let hmac_res = hmac_obj.finalize();
     let mut hmac_res_v: [u8; 64] = [0u8; 64];
+
+    if (num >> 31) == 0 {
+        let sig_params = M31JubJubSigParams::default();
+        let public_key = sig_params.public_key(*private_key);
+        let mut ser: [u8; 33] = [0u8; 33];
+        bincode::serialize_into(&mut ser[1..], &public_key).unwrap();
+//        libc_print::std_name::println!("{:#?}", ser);
+//        public_key.serialize_compressed(&mut ser[..]).unwrap();
+        hmac_obj.update(&ser);
+    } else {
+        let mut source: [u8; 33] = [0u8; 33];
+        assert_eq!(private_key.serialized_size(Compress::Yes), 31);
+        private_key.serialize_compressed(&mut source[1..32]).unwrap();
+        hmac_obj.update(&source);
+    }
+    let num_be = num.to_be_bytes();
+    hmac_obj.update(&num_be);
+    let hmac_res = hmac_obj.finalize();
     hmac_res_v.copy_from_slice(hmac_res.into_bytes().as_slice());
-    libc_print::std_name::println!("hmac_res_v: {:#?}", hmac_res_v);
-    let new_key = Fs::deserialize_with_mode(&hmac_res_v[0..31], Compress::No, Validate::Yes).unwrap();//.ok()?; // TODO: one extra byte !!!!!!
-    let mut new_chain_code: [u8; 32] = [0u8; 32];
-    new_chain_code.copy_from_slice(&hmac_res_v[32..64]);
-    Some((new_key, new_chain_code))
+
+    if let Ok(tweak) = Fs::deserialize_with_mode(&hmac_res_v[0..31], Compress::Yes, Validate::Yes) {// TODO: one extra byte !!!!!!
+        let new_key = tweak + private_key;
+        let mut new_chain_code: [u8; 32] = [0u8; 32];
+        new_chain_code.copy_from_slice(&hmac_res_v[32..64]);
+        Some((new_key, new_chain_code))
+    } else {
+        libc_print::std_name::println!("+1");
+        derive_priv(private_key, chain_code, num+1)
+    }
 }
 
 
@@ -70,7 +96,7 @@ mod tests {
         libc_print::std_name::println!("private_key.serialized_size(): {}", private_key.serialized_size(ark_serialize::Compress::No));
 
 //        assert!(derive_priv(&private_key, MASTER_SEED, ChildNum{ num: 0, is_hardened: true }).is_some());
-        assert!(derive_hardened_priv(&private_key, MASTER_SEED, 0).is_some());
+        assert!(derive_priv(&private_key, MASTER_SEED, 0).is_some());
 
 //        let pka: Vec<u64> = private_key.iter().collect();
 //        let private_key_arr: [u64; 4] = private_key.0.0;
